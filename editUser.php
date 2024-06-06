740fe9b36a7d63f9bea39ef52cde91ddddda7ec6
Kwa2003 <kawakiwoamekuhc@Gmail.com>
2024-06-06 21:29:29 +0800

    not delete user(self)

--- editUser.php
+++ editUser.php
@@ -21,12 +21,13 @@
         $name=$_POST['name'];
         $password=$_POST['password'];
         $permissionlevel=$_POST['permissionlevel'];
+        $flag=$_POST['flag'];
         $casherflag = 0;
         $storeflag = 0;
         $managerflag = 0;
-        if($permissionlevel == 1){
+        if($flag == 1){
             $casherflag = 1;
-        }elseif($permissionlevel == 2){
+        }elseif($flag == 2){
             $storeflag = 1;
         }else{
             $managerflag = 1;
@@ -43,7 +44,7 @@
             echo '<div class="success">Update successfully ！</div>';                
         }
     }elseif(isset($_POST['delete'])){
-        $sql="DELETE FROM `users` WHERE ID=$ID";
+        $sql="DELETE FROM `users` WHERE ID=$UID";
         $result = mysqli_query($db,$sql);
         if (!$result) {
             die('Error: ' . mysqli_error($db));
@@ -66,6 +67,7 @@
         <form name = "editUser" action= "editUser.php" method="post">
         <input type="hidden" name="ID" value="<?=$ID?>">
         <input type="hidden" name="UID" value="<?=$UID?>">
+        <p> ID : <?php echo "$UID" ?> </p>
         <p> 姓名: <input type=text name="name" value="<?php echo $row["name"]?>"></p>
         <p> password: <input type=text name="password" value="<?php echo $row["password"]?>"></p>
         <label for="permissionlevel"> 使用者權限: </label>
@@ -78,10 +80,33 @@
             }
             ?>
         </select>
-        <p><input type="submit" name="change" value="更改"> 
+        <label for="flag"> 職位: </label>
+        <select id="flag" name="flag">
+            <?php 
+            if($row["casherflag"] == 1){
+                $Uflag = 1;
+            }elseif($row["storeflag"] == 1){
+                $Uflag = 2;
+            }else{
+                $Uflag = 3;
+            }
+            for ($i = 1; $i <= 3; $i++){
+                if($i == 1){
+                    $String = "收銀員/銷售人員";
+                }elseif($i == 2){
+                    $String = "倉庫管理員";
+                }else{
+                    $String = "主管";
+                }
+                echo "<option value='$i' ".(($i == $Uflag?'selected="selected"':'')).">
+                $String</option>";
+            }
+            ?>
+        </select>
+        <p><input type="submit" name="change" value="更改">
         <?php
-            if($ID!=$UID){
-                echo'<p><input type="submit" name="delete" value="刪除"> ';
+            if($UID != $ID){
+                echo '<p><input type="submit" name="delete" value="刪除">';
             }
         ?>
         <p><input type="submit" name="back" value="返回">
