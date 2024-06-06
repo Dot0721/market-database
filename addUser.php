47a7f5ae0b6e94f1ce1a1ffe725be72958827cc7
Kwa2003 <kawakiwoamekuhc@Gmail.com>
2024-06-06 21:38:27 +0800

    job function fixed

--- addUser.php
+++ addUser.php
@@ -1,17 +1,26 @@
 <title>addUser</title>
 <?php
     include 'db.php';
-    $ID=$_GET['ID'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $ID=$_POST['ID'];
+        $name=$_POST['name'];
+        $password=$_POST['password'];
+        $permissionlevel=$_POST['permissionlevel'];
+    }
+    else{
+        $ID=$_GET['ID'];
+    }
     if(isset($_POST['add'])){
         $name=$_POST['name'];
         $password=$_POST['password'];
         $permissionlevel=$_POST['permissionlevel'];
+        $flag=$_POST['flag'];
         $casherflag = 0;
         $storeflag = 0;
         $managerflag = 0;
-        if($permissionlevel == 1){
+        if($flag == 1){
             $casherflag = 1;
-        }elseif($permissionlevel == 2){
+        }elseif($flag == 2){
             $storeflag = 1;
         }else{
             $managerflag = 1;
@@ -23,14 +32,20 @@
         } else {
             echo "
                 <script>
-                setTimeout(function(){window.location.href='UserData.php?';},600);
+                setTimeout(function(){window.location.href='UserData.php?ID=".$ID."';},600);
                 </script>";
             echo '<div class="success">Update successfully ！</div>';                
         }
+    }elseif(isset($_POST['back'])){
+        echo "
+        <script>
+        setTimeout(function(){window.location.href='UserData.php?ID=".$ID."';},600);
+        </script>";
     }
 ?>
 <body>
     <form name="addUser" action="addUser.php" method="post">
+    <input type="hidden" name="ID" value="<?=$ID?>">
     <p> 姓名：<input type="text" name="name"></p>
     <p> 密碼：<input type="text" name="password"></p>
     <label for="permissionlevel"> 使用者權限： </label>
@@ -41,5 +56,20 @@
         }
     ?>
     </select>
-    <p><input type="submit" name="add" value="新增使用者">
+    <label for="flag"> 職位: </label>
+    <select id="flag" name="flag">
+        <?php 
+        for ($i = 1; $i <= 3; $i++){
+            if($i == 1){
+                $String = "收銀員/銷售人員";
+            }elseif($i == 2){
+                $String = "倉庫管理員";
+            }else{
+                $String = "主管";
+            }
+            echo "<option value='$i' >$String</option>";
+        }
+        ?>
+    </select>
+    <p><input type="submit" name="add" value="新增使用者"> <p><input type="submit" name="back" value="返回">
 </body>
